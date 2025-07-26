834b2035ca9b75054daf9dc4944dd5ccb0a54559
HusseinMahmoud98 <hussein.mahmoud.fouad98@gmail.com>
2025-07-26 18:13:30 +0300

    Part01

--- Assignment03/Program.cs
+++ Assignment03/Program.cs
@@ -4,7 +4,99 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part01
+
+            #region Q01
+            //Question 1:
+            //What is the primary purpose of an interface in C#?
+            //a) To provide a way to implement multiple inheritance
+            //b) To define a blueprint for a class ============================>[Answer]
+            //c) To declare abstract methods and properties
+            //d) To create instances of objects 
+            #endregion
+
+            #region Q02
+            //Question 2:
+            //Which of the following is NOT a valid access modifier for interface members in C#?
+            //a) private ============================>[Answer]
+            //b) protected
+            //c) internal
+            //d) public 
+            #endregion
+
+            #region Q03
+            //Question 3:
+            //Can an interface contain fields in C#?
+            //a) Yes
+            //b) No ============================>[Answer]
+            //c) Only if they are static
+            //d) Only if they are read only 
+            #endregion
+
+            #region Q04
+            //Question 4:
+            //In C#, can an interface inherit from another interface?
+            //a) No, interfaces cannot inherit from each other 
+            //b) Yes, interfaces can inherit from multiple interfaces  ============================>[Answer]
+            //c) Yes, but only if they have the same methods
+            //d) Only if the interfaces are in the same namespace 
+            #endregion
+
+            #region Q05
+            //Question 5:
+            //Which keyword is used to implement an interface in a class in C#?
+            //a) inherit
+            //b) use
+            //c) extends 
+            //d) implements ============================>[Answer]
+            #endregion
+
+            #region Q06
+            //Question 6:
+            //Can an interface contain static methods in C#?
+            //a) Yes ============================>[Answer]
+            //b) No
+            //c) Only if the interface is sealed
+            //d) Only if the methods are private 
+            #endregion
+
+            #region Q07
+            //Question 7:
+            //In C#, can an interface have explicit access modifiers for its members?
+            //a) Yes, for all members ============================>[Answer]
+            //b) No, all members are implicitly public 
+            //c) Yes, but only for abstract members
+            //d) Only if the interface is sealed 
+            #endregion
+
+            #region Q08
+            //Question 8:
+            //What is the purpose of an explicit interface implementation in C#?
+            //a) To hide the interface members from outside access
+            //b) To provide a clear separation between interface and class members ============================>[Answer]
+            //c) To allow multiple classes to implement the same interface
+            //d) To speed up method resolution 
+            #endregion
+
+            #region Q09
+            //Question 9:
+            //In C#, can an interface have a constructor?
+            //a) Yes, but it must be private
+            //b) No, interfaces cannot have constructors ============================>[Answer]
+            //c) Yes, but only if the interface is sealed
+            //d) Only if the constructor is static 
+            #endregion
+
+            #region Q10
+            //Question 10:
+            //How can a C# class implement multiple interfaces?
+            //a) By using the "implements" keyword
+            //b) By using the "extends" keyword
+            //c) By separating interface names with commas ============================>[Answer]
+            //d) A class cannot implement multiple interfaces  
+            #endregion
+
+            #endregion
         }
     }
 }
